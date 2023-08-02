93e258df9cd38ef2bd28fca088594fd29767ff4b
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-08-02 17:26:43 +0200

    Create Employee_Bad.java

---
+++ Employee_Bad.java
@@ -0,0 +1,40 @@
+package kb;
+
+public class Employee {
+ private double performanceScore;
+ private double bonus;
+
+ public Employee(double performanceScore) {
+     this.performanceScore = performanceScore;
+     this.bonus = 0.0;
+ }
+
+ public void calculateAndApplyAnnualBonus() {
+     // Calculate bonus based on employee's performance score
+     double bonusCalculation = calculateBonusBasedOnPerformance();
+
+     // Apply bonus to employee's record
+     applyBonusToRecord(bonusCalculation);
+ }
+
+ private double calculateBonusBasedOnPerformance() {
+     return performanceScore * 1000.0;
+ }
+
+ private void applyBonusToRecord(double bonus) {
+     // Code to apply bonus to employee's record goes here
+     System.out.println("Bonus applied to the record: " + bonus);
+ }
+
+ public double getBonus() {
+     return bonus;
+ }
+
+ public static void main(String[] args) {
+	 Employee employee = new Employee(8.5);
+
+     employee.calculateAndApplyAnnualBonus();
+
+     System.out.println("Original Bonus: " + employee.getBonus());
+ }
+}
